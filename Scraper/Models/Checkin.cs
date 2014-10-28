5b276ef82884be6cbb143a46f15b02c46f18dc6f
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2014-10-27 21:03:36 -0400

    able to process Untappd pages.

---
+++ Scraper/Models/Checkin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Scraper.Models
+{
+    public class Checkin
+    {
+        public string Id { get; set; }
+        [JsonProperty("beer_name")]
+        public string BeerName { get; set; }
+        [JsonProperty("brewery_name")]
+        public string BreweryName { get; set; }
+        [JsonProperty("beer_type")]
+        public string BeerType { get; set; }
+        [JsonProperty("beer_abv")]
+        public decimal BeerAbv { get; set; }
+        [JsonProperty("beer_ibu")]
+        public decimal BeerIbu { get; set; }
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+        [JsonProperty("venue_name")]
+        public string VenueName { get; set; }
+        [JsonProperty("venue_city")]
+        public string VenueCity { get; set; }
+        [JsonProperty("venue_state")]
+        public string VenueState { get; set; }
+        [JsonProperty("venue_lat")]
+        public decimal? VenueLatitude { get; set; }
+        [JsonProperty("venue_lng")]
+        public decimal? VenueLongitude { get; set; }
+        [JsonProperty("rating_score")]
+        public string RatingScore { get; set; }
+        [JsonProperty("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+        [JsonProperty("checkin_url")]
+        public string CheckinUrl { get; set; }
+        [JsonProperty("beer_url")]
+        public string BeerUrl { get; set; }
+        [JsonProperty("brewery_url")]
+        public string BreweryUrl { get; set; }
+        [JsonProperty("brewery_country")]
+        public string BreweryCountry { get; set; }
+
+        public string BreweryId
+        {
+            get { return BreweryUrl.Replace("https://untappd.com/", ""); }
+        }
+    }
+
+    public class Brewery
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string RawLocation { get; set; }
+        public string Style { get; set; }
+
+        public Brewery ProcessLocation(string location)
+        {
+            if (string.IsNullOrWhiteSpace(location))
+                return this;
+
+            RawLocation = location;
+            try
+            {
+                City = location.Substring(0, location.IndexOf(',')).Trim();
+                var stateAndCountry = location.Replace(City, "").Replace(",", "").Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                State = stateAndCountry.Select(x => x.Trim()).FirstOrDefault();
+                Country = string.Join(" ", stateAndCountry.Skip(1));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(location);
+            }
+            return this;
+        }
+    }
+}
---
+++ Scraper/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coypu;
+using Coypu.Drivers;
+using Coypu.Drivers.Selenium;
+using Newtonsoft.Json;
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Linq;
+using Scraper.Models;
+
+namespace Scraper
+{
+    class Program
+    {
+        private static readonly IDocumentStore Store = new DocumentStore
+        {
+            ConnectionStringName = "RavenDB"
+        }.Initialize(true);
+
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("checkin_export.json");
+            var checkins = JsonConvert.DeserializeObject<List<Checkin>>(data);
+
+            using (var bulk = Store.BulkInsert(options: new BulkInsertOptions { OverwriteExisting = true }))
+            {
+                for (int i = 0; i < checkins.Count; i++)
+                {
+                    var checkin = checkins[i];
+                    checkin.Id = string.Format("checkin-{0}", i);
+                    bulk.Store(checkin);
+                }
+            }
+
+            var uniqueBreweryUrls = checkins.Select(x => x.BreweryUrl).Distinct();
+            var breweries = new List<Brewery>();
+            var configuration = new SessionConfiguration
+            {
+                Driver = typeof(SeleniumWebDriver),
+                Browser = Browser.Chrome
+            };
+
+            using (var browser = new BrowserSession(configuration))
+            {
+                foreach (var unique in uniqueBreweryUrls)
+                {
+                    browser.Visit(unique);
+                    browser.SaveScreenshot("brewery.png", ImageFormat.Png);
+                    var location = browser.FindCss("p.brewery").Text;
+                    var brewery = new Brewery
+                    {
+                        Id = unique.Replace("https://untappd.com/", ""),
+                        Name = browser.FindCss(".basic .name h1").Text.Trim(),
+                        RawLocation = location,
+                        Style = browser.FindCss(".basic .name p.style").Text.Trim()
+                    }.ProcessLocation(location);
+                    breweries.Add(brewery);
+                }
+            }
+
+            using (var bulk = Store.BulkInsert(options: new BulkInsertOptions { OverwriteExisting = true }))
+            {
+                foreach (var brewery in breweries)
+                    bulk.Store(brewery);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
